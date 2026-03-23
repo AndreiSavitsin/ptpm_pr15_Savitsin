493c2f1e4044ee59c3e959300db435be2ce81636
AndreiSavitsin <andreeeeewcs@gmail.com>
2026-03-23 14:03:19 +0500

    Add files via upload

---
+++ prakt15_Savitsin/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt15_Savitsin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room room1 = new Room(10.0, 7.0, 3.0, 2, 1.3, 1.0);
+            Room room2 = new Room(9.0, 8.0, 3.0, 3, 1.3, 1.0);
+            Room room3 = new Room(7.0, 7.0, 3.0, 1, 1.3, 1.0);
+
+            Room.AddRoom(room1);
+            Room.AddRoom(room2);
+            Room.AddRoom(room3);
+
+            Console.WriteLine("Комната 1:");
+            Room.RoomInfo(room1);
+            Console.WriteLine($"Площадь комнаты: {room1.AreaRoom()}");
+            Console.WriteLine($"Объём комнаты: {room1.VolumeRoom()}");
+            Room.RepairRoom(room1);
+            Console.WriteLine();
+
+            Console.WriteLine("Комната 2:");
+            Room.RoomInfo(room2);
+            Console.WriteLine($"Площадь комнаты: {room2.AreaRoom()}");
+            Console.WriteLine($"Объём комнаты: {room2.VolumeRoom()}");
+            Room.RepairRoom(room2);
+            Console.WriteLine();
+
+            Console.WriteLine("Комната 3:");
+            Room.RoomInfo(room3);
+            Console.WriteLine($"Площадь комнаты: {room3.AreaRoom()}");
+            Console.WriteLine($"Объём комнаты: {room3.VolumeRoom()}");
+            Room.RepairRoom(room3);
+            Console.WriteLine();
+
+            Console.WriteLine($"Площадь всех окон в помещении: {Room.AreaAllWindow()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ prakt15_Savitsin/Room.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt15_Savitsin
+{
+    class Room
+    {
+        static List<Room> RoomList = new List<Room>();
+
+        double lengthRoom;
+        double widthRoom;
+        double heightRoom;
+        int countWindow;
+        double heightWindow;
+        double widthWindow;
+
+        public Room(double lengthRoom, double widthRoom, double heightRoom, int countWindow, double heightWindow, double widthWindow)
+        {
+            this.lengthRoom = lengthRoom; //Длина комнаты
+            this.widthRoom = widthRoom; //Ширина комнаты
+            this.heightRoom = heightRoom; //Высота комнаты
+            this.countWindow = countWindow; //Количество окон
+            this.heightWindow = heightWindow; //Высота окон
+            this.widthWindow = widthWindow; //Ширина окон
+        }
+
+        public double LengthRoom
+        {
+            get { return lengthRoom; }
+            set { lengthRoom = value; }
+        }
+        public double WidthRoom
+        {
+            get { return widthRoom; }
+            set { widthRoom = value; }
+        }
+        public double HeightRoom
+        {
+            get { return heightRoom; }
+            set { heightRoom = value; }
+        }
+        public int CountWindow
+        {
+            get { return countWindow; }
+            set { countWindow = value; }
+        }
+        public double HeightWindow
+        {
+            get { return heightWindow; }
+            set { heightWindow = value; }
+        }
+        public double WidthWindow
+        {
+            get { return widthWindow; }
+            set { widthWindow = value; }
+        }
+        static public void AddRoom(Room room) //Добавление комнаты в лист
+        {
+            RoomList.Add(room);
+        }
+        public double AreaRoom() //Площадь комнаты
+        {
+            return lengthRoom * widthRoom;
+        }
+        public double VolumeRoom() //Объём комнаты
+        {
+            return lengthRoom * widthRoom * heightRoom;
+        }
+        static public void RoomInfo(Room room) //Информация о комнате
+        {
+            Console.WriteLine($"Длина комнаты: {room.lengthRoom}. Ширина комнаты: {room.widthRoom}. Высота комнаты: {room.heightRoom}." +
+                $"Количество окон: {room.countWindow}. Высота окон: {room.heightWindow}. Ширина окон: {room.widthWindow}");
+        }
+        static public void ListRoomInfo()
+        {
+            foreach (var item in RoomList)
+            {
+                Console.WriteLine($"Длина комнаты: {item.lengthRoom}. Ширина комнаты: {item.widthRoom}. Высота комнаты: {item.heightRoom}." +
+                $"Количество окон: {item.countWindow}. Высота окон: {item.heightWindow}. Ширина окон: {item.widthWindow}");
+            }
+        }
+        static public void RepairRoom(Room room) //Количество рулонов обоев
+        {
+            try
+            {
+                Console.Write("Введите ширину рулона: ");
+                double widthRoll = Convert.ToDouble(Console.ReadLine());
+                if (widthRoll <= 0.0)
+                {
+                    Console.WriteLine("Ширина рулона не может быть <= 0");
+                }
+                else
+                {
+                    double wall1 = room.heightRoom * room.lengthRoom;
+                    double wall2 = room.heightRoom * room.widthRoom;
+                    double AreaWallsRoom = 2 * (wall1 + wall2);
+
+                    double AreaRoll1 = 10 * widthRoll;
+                    double AreaRoll2 = 15 * widthRoll;
+
+                    double countRoll1 = AreaWallsRoom / AreaRoll1;
+                    double countRoll2 = AreaWallsRoom / AreaRoll2;
+
+                    Console.WriteLine($"Рулон: длина = 10, ширина = {widthRoll}");
+                    Console.WriteLine($"Количество рулонов: {countRoll1}");
+                    Console.WriteLine($"Рулон: длина = 15, ширина = {widthRoll}");
+                    Console.WriteLine($"Количество рулонов: {countRoll2}");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверный формат значения");
+            }
+        }
+        static public double AreaAllWindow() //Площадь всех окон
+        {
+            double areaAllWindowAllRoom = 0;
+            foreach (var item in RoomList)
+            {
+                double areaWindow = item.HeightWindow * item.WidthWindow;
+                double areaAllWindowOneRoom = item.CountWindow * areaWindow;
+                areaAllWindowAllRoom += areaAllWindowOneRoom;
+            }
+            return areaAllWindowAllRoom;
+        }
+    }
+}
