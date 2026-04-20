c2ce995892a950f5130599462e1c9fbf3413261d
AndreiSavitsin <andreeeeewcs@gmail.com>
2026-04-20 15:39:41 +0500

    Add files via upload

---
+++ prakt15_Savitsin/Room.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt15_Savitsin
+{
+    public class Room
+    {
+        static public List<Room> RoomList = new List<Room>();
+        static string filePath = "RoomInfo.txt";
+
+        double lengthRoom;
+        double widthRoom;
+        double heightRoom;
+        int countWindow;
+        double heightWindow;
+        double widthWindow;
+
+        public Room(double lengthRoom, double widthRoom, double heightRoom, int countWindow, double heightWindow, double widthWindow)
+        {
+            this.lengthRoom = lengthRoom; //Длина комнаты
+            this.widthRoom = widthRoom; //Ширина комнаты
+            this.heightRoom = heightRoom; //Высота комнаты
+            this.countWindow = countWindow; //Количество окон
+            this.heightWindow = heightWindow; //Высота окон
+            this.widthWindow = widthWindow; //Ширина окон
+        }
+
+        public double LengthRoom
+        {
+            get { return lengthRoom; }
+            set { lengthRoom = value; }
+        }
+        public double WidthRoom
+        {
+            get { return widthRoom; }
+            set { widthRoom = value; }
+        }
+        public double HeightRoom
+        {
+            get { return heightRoom; }
+            set { heightRoom = value; }
+        }
+        public int CountWindow
+        {
+            get { return countWindow; }
+            set { countWindow = value; }
+        }
+        public double HeightWindow
+        {
+            get { return heightWindow; }
+            set { heightWindow = value; }
+        }
+        public double WidthWindow
+        {
+            get { return widthWindow; }
+            set { widthWindow = value; }
+        }
+        static public void AddRoom(Room room) //Добавление комнаты в лист
+        {
+            RoomList.Add(room);
+        }
+        public double AreaRoom() //Площадь комнаты
+        {
+            return lengthRoom * widthRoom;
+        }
+        public double VolumeRoom() //Объём комнаты
+        {
+            return lengthRoom * widthRoom * heightRoom;
+        }
+        static public string RoomInfo(Room room) //Информация о комнате
+        {
+            return $"Длина комнаты: {room.lengthRoom} Ширина комнаты: {room.widthRoom} Высота комнаты: {room.heightRoom} Количество окон: {room.countWindow} Высота окон: {room.heightWindow} Ширина окон: {room.widthWindow}";
+        }
+        static public List<string> ListRoomInfo()
+        {
+            List<string> room = new List<string>();
+            foreach (var item in RoomList)
+            {
+                room.Add($"Длина комнаты: {item.lengthRoom} Ширина комнаты: {item.widthRoom} Высота комнаты: {item.heightRoom} Количество окон: {item.countWindow} Высота окон: {item.heightWindow} Ширина окон: {item.widthWindow}");
+            }
+            return room;
+        }
+        static public void FillInfo()
+        {
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                foreach (var item in RoomList)
+                {
+                    sw.WriteLine($"Длина комнаты: {item.lengthRoom} Ширина комнаты: {item.widthRoom} Высота комнаты: {item.heightRoom} Количество окон: {item.countWindow} Высота окон: {item.heightWindow} Ширина окон: {item.widthWindow}");
+                }
+            }
+        }
+        static public string RepairRoom(Room room) //Количество рулонов обоев (для консоли)
+        {
+            try
+            {
+                Console.Write("Введите ширину рулона: ");
+                double widthRoll = Convert.ToDouble(Console.ReadLine());
+                if (widthRoll <= 0.0)
+                {
+                    return "Ширина рулона не может быть <= 0";
+                }
+                else
+                {
+                    double wall1 = room.heightRoom * room.lengthRoom;
+                    double wall2 = room.heightRoom * room.widthRoom;
+                    double AreaWallsRoom = 2 * (wall1 + wall2);
+
+                    double AreaRoll1 = 10 * widthRoll;
+                    double AreaRoll2 = 15 * widthRoll;
+
+                    double countRoll1 = AreaWallsRoom / AreaRoll1;
+                    double countRoll2 = AreaWallsRoom / AreaRoll2;
+
+                    return $"Рулон: длина = 10 ширина = {widthRoll}\n" + $"Количество рулонов: {Math.Ceiling(countRoll1)}\n" 
+                        + $"Рулон: длина = 15 ширина = {widthRoll}\n" + $"Количество рулонов: {Math.Ceiling(countRoll2)}\n";
+                }
+            }
+            catch
+            {
+                return "Неверный формат значения";
+            }
+        }
+        static public string RepairRoom(double width, Room room) //Количество рулонов обоев (для форма)
+        {
+            double wall1 = room.heightRoom * room.lengthRoom;
+            double wall2 = room.heightRoom * room.widthRoom;
+            double AreaWallsRoom = 2 * (wall1 + wall2);
+
+            double AreaRoll1 = 10 * width;
+            double AreaRoll2 = 15 * width;
+
+            double countRoll1 = AreaWallsRoom / AreaRoll1;
+            double countRoll2 = AreaWallsRoom / AreaRoll2;
+
+            return $"Рулон: длина = 10 ширина = {width} " + $"Количество рулонов: {Math.Ceiling(countRoll1)} \n"
+                + $"Рулон: длина = 15 ширина = {width} " + $"Количество рулонов: {Math.Ceiling(countRoll2)}";
+        }
+        static public double AreaAllWindow() //Площадь всех окон
+        {
+            double areaAllWindowAllRoom = 0;
+            foreach (var item in RoomList)
+            {
+                double areaWindow = item.HeightWindow * item.WidthWindow;
+                double areaAllWindowOneRoom = item.CountWindow * areaWindow;
+                areaAllWindowAllRoom += areaAllWindowOneRoom;
+            }
+            return areaAllWindowAllRoom;
+        }
+    }
+}
---
+++ ptpm_pr15_form_Savitsin/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using prakt15_Savitsin;
+
+namespace ptpm_pr15_form_Savitsin
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "RoomInfo.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            LoadFromFile();
+            FillDataGrid();
+        }
+
+        private void LoadFromFile() //Загрузка из текстового файла
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string str = sr.ReadLine();
+                    string[] parts = str.Split(' ');
+                    Room room = new Room(0, 0, 0, 0, 0, 0);
+                    if (!double.TryParse(parts[2], out double lengthRoom))
+                    {
+                        room.LengthRoom = 5.0;
+                    }
+                    else
+                    {
+                        if (lengthRoom <= 0)
+                        {
+                            room.LengthRoom = 5.0;
+                        }
+                        else
+                        {
+                            room.LengthRoom = lengthRoom;
+                        }
+                    }
+
+                    if (!double.TryParse(parts[5], out double widthRoom))
+                    {
+                        room.WidthRoom = 5.0;
+                    }
+                    else
+                    {
+                        if (widthRoom <= 0)
+                        {
+                            room.WidthRoom = 5.0;
+                        }
+                        else
+                        {
+                            room.WidthRoom = widthRoom;
+                        }
+                    }
+
+                    if (!double.TryParse(parts[8], out double heightRoom))
+                    {
+                        room.HeightRoom = 5.0;
+                    }
+                    else
+                    {
+                        if (heightRoom <= 0)
+                        {
+                            room.HeightRoom = 5.0;
+                        }
+                        else
+                        {
+                            room.HeightRoom = heightRoom;
+                        }
+                    }
+
+                    if (!int.TryParse(parts[11], out int countWindow))
+                    {
+                        room.CountWindow = 1;
+                    }
+                    else
+                    {
+                        if (countWindow <= 0)
+                        {
+                            room.CountWindow = 1;
+                        }
+                        else
+                        {
+                            room.CountWindow = countWindow;
+                        }
+                    }
+
+                    if (!double.TryParse(parts[14], out double heightWindow))
+                    {
+                        room.HeightWindow = 2;
+                    }
+                    else
+                    {
+                        if (heightWindow <= 0)
+                        {
+                            room.HeightWindow = 2;
+                        }
+                        else
+                        {
+                            room.HeightWindow = heightWindow;
+                        }
+                    }
+
+                    if (!double.TryParse(parts[17], out double widthWindow))
+                    {
+                        room.WidthWindow = 2;
+                    }
+                    else
+                    {
+                        if (widthWindow <= 0)
+                        {
+                            room.WidthWindow = 2;
+                        }
+                        else
+                        {
+                            room.WidthWindow = widthWindow;
+                        }
+                    }
+
+                    Room.RoomList.Add(room);
+                }
+            }
+        }
+        private void FillDataGrid() //Заполнение dataGridView
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn() { Name = "lengthRoom", HeaderText = "Длина комнаты" };
+            DataGridViewTextBoxColumn column2 = new DataGridViewTextBoxColumn() { Name = "widthRoom", HeaderText = "Ширина комнаты" };
+            DataGridViewTextBoxColumn column3 = new DataGridViewTextBoxColumn() { Name = "heightRoom", HeaderText = "Высота комнаты" };
+            DataGridViewTextBoxColumn column4 = new DataGridViewTextBoxColumn() { Name = "countWindow", HeaderText = "Количество окон" };
+            DataGridViewTextBoxColumn column5 = new DataGridViewTextBoxColumn() { Name = "heightWindow", HeaderText = "Высота окон" };
+            DataGridViewTextBoxColumn column6 = new DataGridViewTextBoxColumn() { Name = "widthWindow", HeaderText = "Ширина окон" };
+
+            dataGridView1.Columns.AddRange(new DataGridViewTextBoxColumn[] { column1, column2, column3, column4, column5, column6});
+
+
+            for (int i = 0; i < Room.RoomList.Count; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1[0, i].Value = Room.RoomList[i].LengthRoom;
+                dataGridView1[1, i].Value = Room.RoomList[i].WidthRoom;
+                dataGridView1[2, i].Value = Room.RoomList[i].HeightRoom;
+                dataGridView1[3, i].Value = Room.RoomList[i].CountWindow;
+                dataGridView1[4, i].Value = Room.RoomList[i].HeightWindow;
+                dataGridView1[5, i].Value = Room.RoomList[i].WidthWindow;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) //Кнопка создать комнату
+        {
+            Room room = new Room(0, 0, 0, 0, 0, 0);
+            if (!double.TryParse(txtLengthRoom.Text, out double lengthRoom))
+            {
+                room.LengthRoom = 5.0;
+            }
+            else
+            {
+                if (lengthRoom <= 0)
+                {
+                    room.LengthRoom = 5.0;
+                }
+                else
+                {
+                    room.LengthRoom = lengthRoom;
+                }
+            }
+
+            if (!double.TryParse(txtWidthRoom.Text, out double widthRoom))
+            {
+                room.WidthRoom = 5.0;
+            }
+            else
+            {
+                if (widthRoom <= 0)
+                {
+                    room.WidthRoom = 5.0;
+                }
+                else
+                {
+                    room.WidthRoom = widthRoom;
+                }
+            }
+
+            if (!double.TryParse(txtHeightRoom.Text, out double heightRoom))
+            {
+                room.HeightRoom = 5.0;
+            }
+            else
+            {
+                if (heightRoom <= 0)
+                {
+                    room.HeightRoom = 5.0;
+                }
+                else
+                {
+                    room.HeightRoom = heightRoom;
+                }
+            }
+
+            if (!int.TryParse(txtCountWindow.Text, out int countWindow))
+            {
+                room.CountWindow = 1;
+            }
+            else
+            {
+                if (countWindow <= 0)
+                {
+                    room.CountWindow = 1;
+                }
+                else
+                {
+                    room.CountWindow = countWindow;
+                }
+            }
+
+            if (!double.TryParse(txtHeightWindow.Text, out double heightWindow))
+            {
+                room.HeightWindow = 2;
+            }
+            else
+            {
+                if (heightWindow <= 0)
+                {
+                    room.HeightWindow = 2;
+                }
+                else
+                {
+                    room.HeightWindow = heightWindow;
+                }
+            }
+
+            if (!double.TryParse(txtWidthWindow.Text, out double widthWindow))
+            {
+                room.WidthWindow = 2;
+            }
+            else
+            {
+                if (widthWindow <= 0)
+                {
+                    room.WidthWindow = 2;
+                }
+                else
+                {
+                    room.WidthWindow = widthWindow;
+                }
+            }
+
+            Room.AddRoom(room);
+            FillDataGrid();
+
+            double widthRoll;
+            if (!double.TryParse(txtWidthRoll.Text, out double wRoll))
+            {
+                widthRoll = 1;
+            }
+            else
+            {
+                widthRoll = wRoll;
+            }
+
+            string info1 = Room.RepairRoom(widthRoll, room);
+            double info2 = Room.AreaAllWindow();
+
+            MessageBox.Show(info1, "Информация о рулонах", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show($"Площадь всех окон: {info2}", "Информация о площади", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            Room.FillInfo();
+        }
+    }
+}
